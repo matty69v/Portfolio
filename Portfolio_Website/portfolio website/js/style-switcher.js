8fc9f9a4cbff1647bec7d9fa5ca13d3f9e179a74
Mattéo Bezet-Torres <m.bezettorres@eleve.leschartreux.net>
2024-10-06 18:18:31 +0200

    meh

--- Portfolio_Website/portfolio website/js/style-switcher.js
+++ Portfolio_Website/portfolio website/js/style-switcher.js
@@ -31,14 +31,14 @@ dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark");
+    if(document.body.classList.contains("dark")) {
+        setActiveStyle("default"); // Assuming "default" is the title for the black theme
+    } else {
+        setActiveStyle("white"); // Assuming "white" is the title for the white theme
+    }
 })
 window.addEventListener("load", () => {
-    if(document.body.classList.contains("dark"))
-    {
-        dayNight.querySelector("i").classList.add("fa-sun");
-    }
-    else
-    {
-        dayNight.querySelector("i").classList.add("fa-moon");
-    }
+    document.body.classList.add("dark");
+    dayNight.querySelector("i").classList.add("fa-sun");
+    setActiveStyle("default"); // Set default theme to black on load
 })
